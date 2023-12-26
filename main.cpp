6ccef84bc42ac02604cb9393566523636d2e9cf1
Amanksh <amankushwaha933@gmail.com>
2023-12-26 09:55:21 +0530

    added graphics

--- main.cpp
+++ main.cpp
@@ -1,15 +1,50 @@
 #include <iostream>
 #include <raylib.h>
 using namespace std;
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+int cellSize = 30;
+int cellCount = 25;
 
+// Food object
+class Food
+{
+public:
+    Vector2 position;
+    Texture2D texture;
+    Food()
+    {
+        Image image = LoadImage("Graphics/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPos();
+    }
+    ~Food()
+    {
+        UnloadTexture(texture);
+    }
+    void Draw()
+    {
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+    Vector2 GenerateRandomPos()
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x, y};
+    }
+};
 int main()
 {
-    InitWindow(750, 750, "retro snake game");
+    InitWindow(cellCount * cellCount, cellCount * cellSize, "retro snake game");
     SetTargetFPS(60);
+    Food food = Food();
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
-
+        // drawing
+        ClearBackground(green);
+        food.Draw();
         EndDrawing();
     }
     CloseWindow();
